15933f1ca0afdd80ac530ba1a541ec427f9c0981
Martin <martinmdvld@gmail.com>
2022-02-28 13:48:35 +0100

    First commit navbar finie

---
+++ resources/views/partial/navbar.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <nav class="navbar-main">
+        <div class="logo">
+            <i class="fas fa-rocket"></i>
+        </div>
+        <div class="toggle">
+            <i class="fas fa-bars ouvrir"></i>
+            <i class="fas fa-times fermer"></i>
+        </div>
+        <ul class="ul-navbar">
+            <div class="div-lien">
+                <li class="s1"><a href="" class="a-lien">Home</a></li>
+                <li class="s2"><a href="" class="a-lien">Home</a></li>
+            </div>
+            <div class="div-lien">
+                <li class="s1"><a href="" class="a-lien">A propos</a></li>
+                <li class="s2"><a href="" class="a-lien">A propos</a></li>
+            </div>
+            <div class="div-lien">
+                <li class="s1"><a href="" class="a-lien">Nos services</a></li>
+                <li class="s2"><a href="" class="a-lien">Nos services</a></li>
+            </div>
+            <div class="div-lien">
+                <li class="s1"><a href="" class="a-lien">Projets</a></li>
+                <li class="s2"><a href="" class="a-lien">Projets</a></li>
+            </div>
+            <div class="div-lien">
+                <li class="s1"><a href="" class="a-lien">Contact</a></li>
+                <li class="s2"><a href="" class="a-lien">Contact</a></li>
+            </div>
+        </ul>
+    </nav>
+</header>
