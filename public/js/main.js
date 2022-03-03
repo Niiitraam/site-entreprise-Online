e8877e78d014069ab57e9f4e7bdd97804f64cbb5
Martin <martinmdvld@gmail.com>
2022-03-03 04:03:59 +0100

    about modif et service modif

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,30 @@
+//navbar
+
 let toggle = document.querySelector('.toggle')
 let body = document.querySelector('body')
 
 toggle.addEventListener('click', function(){
     body.classList.toggle('open')
 })
+
+//card service
+
+let card = document.getElementsByClassName('location')
+let cardBox = Array.from(card)
+
+cardBox.forEach((element, index) =>{
+    element.addEventListener('mouseover', function(){
+        toucher(index)
+    })
+    element.addEventListener('mouseout', function(){
+        toucherOut(index)
+    })
+})
+
+function toucher(i){
+    cardBox[i].classList.add('flip')
+}
+
+function toucherOut(i){
+    cardBox[i].classList.remove('flip')
+}
--- resources/views/page/service.blade.php
+++ resources/views/page/service.blade.php
@@ -5,25 +5,49 @@ We create solutions that deliver tangible business results --}}
     <h2 class="h2-service jaune">Nos services</h2>
     <div class="div-carte">
         <div class="location carte1">
-            <img src="{{asset('image/location.png')}}" class="img-location" alt="">
-            <p class="text-location jaune">Site vitrine</p>
+            <div class="carte-face carte-front">
+                <img src="{{asset('image/location.png')}}" class="img-location" alt="">
+                <p class="text-location jaune">Site vitrine</p>
+            </div>
+            <div class="carte-face carte-back">
+                <p class="jaune">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quae aliquid iure sequi. Nobis enim voluptatum temporibus odit molestiae, voluptates laboriosam, mollitia ad eaque aperiam culpa ullam eos vel qui.</p>
+            </div>
         </div>
         <div class="location margin-about carte2">
-            <img src="{{asset('image/location.png')}}" class="img-location" alt="">
-            <p class="text-location jaune">Site E-commerce</p>
+            <div class="carte-face carte-front">
+                <img src="{{asset('image/location.png')}}"          class="img-location" alt="">
+                <p class="text-location jaune">Site E-commerce</p>
+            </div>
+            <div class="carte-face carte-back">
+                <p class="jaune">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quae aliquid iure sequi. Nobis enim voluptatum temporibus odit molestiae, voluptates laboriosam, mollitia ad eaque aperiam culpa ullam eos vel qui.</p>
+            </div>
         </div>
-        <div class="location margin-about">
-            <img src="{{asset('image/location.png')}}" class="img-location" alt="">
-            <p class="text-location jaune">Mise à jour</p>
+        <div class="location margin-about exeption">
+            <div class="carte-face carte-front">
+                <img src="{{asset('image/location.png')}}"          class="img-location" alt="">
+                <p class="text-location jaune">Mise à jour</p>
+            </div>
+            <div class="carte-face carte-back">
+                <p class="jaune">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quae aliquid iure sequi. Nobis enim voluptatum temporibus odit molestiae, voluptates laboriosam, mollitia ad eaque aperiam culpa ullam eos vel qui.</p>
+            </div>
         </div>
         <div class="location margin-about carte3">
-            <img src="{{asset('image/location.png')}}" class="img-location" alt="">
-            <p class="text-location jaune">Wordpress</p>
+            <div class="carte-face carte-front">
+                <img src="{{asset('image/location.png')}}"          class="img-location" alt="">
+                <p class="text-location jaune">Wordpress</p>
+            </div>
+            <div class="carte-face carte-back">
+                <p class="jaune">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quae aliquid iure sequi. Nobis enim voluptatum temporibus odit molestiae, voluptates laboriosam, mollitia ad eaque aperiam culpa ullam eos vel qui.</p>
+            </div>
         </div>
         <div class="location margin-about carte4">
-            <img src="{{asset('image/location.png')}}" class="img-location" alt="">
-            <p class="text-location jaune">Refonte</p>
+            <div class="carte-face carte-front">
+                <img src="{{asset('image/location.png')}}"          class="img-location" alt="">
+                <p class="text-location jaune">Refonte de site</p>
+            </div>
+            <div class="carte-face carte-back">
+                <p class="jaune">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quae aliquid iure sequi. Nobis enim voluptatum temporibus odit molestiae, voluptates laboriosam, mollitia ad eaque aperiam culpa ullam eos vel qui.</p>
+            </div>
         </div>
     </div>
-    
 </div>
