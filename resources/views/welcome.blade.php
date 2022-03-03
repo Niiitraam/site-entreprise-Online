98a0e1138222593a5dd70b268fc717180903b2cd
Martin <martinmdvld@gmail.com>
2022-03-03 04:13:11 +0100

    Procfile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/6434cb6a94.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/style.css')}}">
     
     <title>Main</title>
 </head>
@@ -19,6 +19,6 @@
     @include('partial/reseau')
 
 
-    <script src="{{asset('js/main.js')}}"></script>
+    <script src="{{secure_asset('js/main.js')}}"></script>
 </body>
 </html>
