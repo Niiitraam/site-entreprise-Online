52bcab794f66e394921ebd31a8d8c568618eaee5
Niiitraam <martinmdvld@gmail.com>
2022-08-22 12:06:49 +0200

    mise a jour

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/6434cb6a94.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/style.css')}}">
 
     <title>Martin Manderveld</title>
 </head>
